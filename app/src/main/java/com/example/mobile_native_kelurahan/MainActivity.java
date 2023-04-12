c740a0123df474cc0ddd58e35546be254f54a686
FaisalOktabrianSholihin <faisalokta08@gmail.com>
2023-04-12 23:58:59 +0700

    update logins

--- app/src/main/java/com/example/mobile_native_kelurahan/MainActivity.java
+++ app/src/main/java/com/example/mobile_native_kelurahan/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.mobile_native_kelurahan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText usernamek;
+    EditText password;
+    Button loginButton;
+//    TextView signupText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usernamek = findViewById(R.id.usernamek);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+//        signupText = findViewById(R.id.signupText);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (usernamek.getText().toString().equals("111") && password.getText().toString().equals("111")) {
+                    Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+//        signupText.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(MainActivity.this, Registrasi.class));
+//            }
+//        });
     }
 }
