5d765ef641bcc2972f31cd8cbf28c9fbf7e7e220
FaisalOktabrianSholihin <faisalokta08@gmail.com>
2023-04-19 15:59:31 +0700

    profile

---
+++ app/src/main/java/com/example/mobile_native_kelurahan/profil.java
@@ -0,0 +1,30 @@
+package com.example.mobile_native_kelurahan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class profil extends AppCompatActivity {
+
+    Button btn_logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+
+        btn_logout = findViewById(R.id.btn_logout);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(profil.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
