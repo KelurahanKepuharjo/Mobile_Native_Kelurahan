1cb60eb9c094fbf25364f7520acf28b972553530
FaisalOktabrianSholihin <faisalokta08@gmail.com>
2023-04-26 01:55:30 +0700

    update fragment status custom button

--- app/src/main/java/com/example/mobile_native_kelurahan/NotificationFragment.java
+++ app/src/main/java/com/example/mobile_native_kelurahan/NotificationFragment.java
@@ -1,5 +1,7 @@
 package com.example.mobile_native_kelurahan;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +11,8 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.TextView;
 
 /**
@@ -48,33 +52,27 @@ public class NotificationFragment extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
-    private TextView antrian, proses, selesai;
-    private int selectedTabNumber = 1;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
 
     }
-
+    private TextView antrian, proses, selesai;
+    private int selectedTabNumber = 1;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_notification, container, false);
 
         antrian = view.findViewById(R.id.antrian);
         proses = view.findViewById(R.id.proses);
         selesai = view.findViewById(R.id.selesai);
-
-//        getSupportFragmentManager().beginTransaction()
-//                .setReorderingAllowed(true)
-//                .replace(R.id.fragmentContainer, HomeFragment.class, null)
-//                .commit();
 
         FragmentManager fragmentManager = getChildFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -86,6 +84,7 @@ public class NotificationFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
+                selectTab(1);
             }
         });
 
@@ -93,6 +92,7 @@ public class NotificationFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
+                selectTab(2);
             }
         });
 
@@ -100,6 +100,7 @@ public class NotificationFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
+                selectTab(3);
             }
         });
 
@@ -108,6 +109,93 @@ public class NotificationFragment extends Fragment {
 
     private void selectTab(int tabNumber){
 
+        TextView selectedTextView;
+
+        TextView nonSelectedTextView1;
+        TextView nonSelectedTextView2;
+
+        if (tabNumber == 1){
+
+            selectedTextView = antrian;
+
+            nonSelectedTextView1 = proses;
+            nonSelectedTextView2 = selesai;
+
+            FragmentManager fragmentManager = getChildFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            antrianFragment antrianFragment = new antrianFragment();
+            fragmentTransaction.add(R.id.fragmentContainer1, antrianFragment);
+            fragmentTransaction.commit();
+        }
+        else if (tabNumber == 2){
+            selectedTextView = proses;
+
+            nonSelectedTextView1 = antrian;
+            nonSelectedTextView2 = selesai;
+
+            FragmentManager fragmentManager = getChildFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            prosesFragment prosesFragment = new prosesFragment();
+            fragmentTransaction.add(R.id.fragmentContainer1, prosesFragment);
+            fragmentTransaction.commit();
+        }
+        else {
+            selectedTextView = selesai;
+
+            nonSelectedTextView1 = antrian;
+            nonSelectedTextView2 = proses;
+
+            FragmentManager fragmentManager = getChildFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            selesaiFragment selesaiFragment = new selesaiFragment();
+            fragmentTransaction.add(R.id.fragmentContainer1, selesaiFragment);
+            fragmentTransaction.commit();
+        }
+
+        float slideTo = (tabNumber - selectedTabNumber) * selectedTextView.getWidth();
+
+        TranslateAnimation translateAnimation = new TranslateAnimation(0, slideTo, 0,0);
+        translateAnimation.setDuration(100);
+
+        if (selectedTabNumber == 1){
+            antrian.startAnimation(translateAnimation);
+        }
+        else if (selectedTabNumber == 2){
+            proses.startAnimation(translateAnimation);
+        }
+        else {
+            selesai.startAnimation(translateAnimation);
+        }
+
+        translateAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                selectedTextView.setBackgroundResource(R.drawable.bkg_putih_100);
+                selectedTextView.setTypeface(null, Typeface.BOLD);
+                selectedTextView.setTextColor(Color.BLACK);
+
+                nonSelectedTextView1.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                nonSelectedTextView1.setTextColor(Color.parseColor("#FF2A2A72"));
+                nonSelectedTextView1.setTypeface(null, Typeface.NORMAL);
+
+                nonSelectedTextView2.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                nonSelectedTextView2.setTextColor(Color.parseColor("#FF2A2A72"));
+                nonSelectedTextView2.setTypeface(null, Typeface.NORMAL);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        selectedTabNumber = tabNumber;
     }
 
 }
